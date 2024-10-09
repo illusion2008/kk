0f2c2645353091a3720a526074d58ba263b38c78
illusion2008 <paulisahak@gmail.com>
2024-10-09 07:20:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+function rollDice() {
+    const result = Math.floor(Math.random() * 6) + 1;
+    document.getElementById('result').textContent = result;
+}
+
+const trucks = document.querySelectorAll('.draggable-truck');
+
+trucks.forEach(truck => {
+    let isDragging = false;
+    let currentX;
+    let currentY;
+    let initialX;
+    let initialY;
+    let xOffset = 0;
+    let yOffset = 0;
+
+    truck.addEventListener('mousedown', dragStart);
+    truck.addEventListener('touchstart', dragStart, { passive: false });
+
+    function dragStart(e) {
+        if (e.type === 'touchstart') {
+            initialX = e.touches[0].clientX - xOffset;
+            initialY = e.touches[0].clientY - yOffset;
+        } else {
+            initialX = e.clientX - xOffset;
+            initialY = e.clientY - yOffset;
+        }
+
+        isDragging = true;
+        truck.style.cursor = 'grabbing';
+
+        document.addEventListener('mousemove', drag);
+        document.addEventListener('touchmove', drag, { passive: false });
+        document.addEventListener('mouseup', dragEnd);
+        document.addEventListener('touchend', dragEnd);
+    }
+
+    function drag(e) {
+        if (isDragging) {
+            e.preventDefault();
+            if (e.type === 'touchmove') {
+                currentX = e.touches[0].clientX - initialX;
+                currentY = e.touches[0].clientY - initialY;
+            } else {
+                currentX = e.clientX - initialX;
+                currentY = e.clientY - initialY;
+            }
+
+            xOffset = currentX;
+            yOffset = currentY;
+
+            setTranslate(currentX, currentY, truck);
+        }
+    }
+
+    function dragEnd(e) {
+        isDragging = false;
+        truck.style.cursor = 'move';
+
+        document.removeEventListener('mousemove', drag);
+        document.removeEventListener('touchmove', drag);
+        document.removeEventListener('mouseup', dragEnd);
+        document.removeEventListener('touchend', dragEnd);
+    }
+
+    function setTranslate(xPos, yPos, el) {
+        el.style.transform = `translate3d(${xPos}px, ${yPos}px, 0)`;
+    }
+});
